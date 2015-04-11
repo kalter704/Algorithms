1b5ed80af3b495e6d6b70bb78f16134d7862ac00
kalter704 <lantratov.vasia@yandex.ru>
2015-04-11 15:13:28 +0300

    Next

--- Module_2/M2_Z4_V3/M2_Z4_V3_quick_sort_temp.cpp
+++ Module_2/M2_Z4_V3/M2_Z4_V3_quick_sort_temp.cpp
@@ -1,5 +1,20 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+
+template <typename data_t>
+void dump(data_t per) {
+	std::cout << per << std::endl;
+}
+
+template <typename data_t>
+void dump_mas(data_t *mas, size_t n) {
+	std::cout << '[';
+	for(int i = 0; i < n; ++i) {
+		std::cout << mas[i];
+	}
+	std::cout << ']' << std::endl;
+}
 
 bool less(const int &lhs, const int	&rhs) {
 	return lhs <= rhs;
@@ -21,6 +36,7 @@ size_t distance(data_t *begin, data_t *end) {
 
 template <typename data_t, typename less_t>
 size_t split(data_t *mas, size_t n, less_t &less) {
+	//size_t pivot = n - 1;
 	size_t i = 0,
 				 j = 0;
 	while(j < n - 1) {
@@ -32,18 +48,27 @@ size_t split(data_t *mas, size_t n, less_t &less) {
 			j++;
 		}
 	}
+	//dump_mas(mas, n);
 	swap(mas[i], mas[n - 1]);
+	//dump_mas(mas, n);
 	return i;
 }
 
 template <typename data_t, typename less_t>
 void quick_sort(data_t *mas, size_t n, less_t &less) {
+	//size_t dist = distance(begin, end);
+	//printf("\n%d\n", dist);
 	if(n < 2) {
 		return;
 	}
 	int index = rand() % n;
+	//dump(index);
+	//dump_mas(mas, n);
 	swap(mas[index], mas[n - 1]);
+	//dump_mas(mas, n);
 	size_t pivot = split(mas, n, less);
+	//dump(mas[pivot]);
+	//std::cout << std::endl;
 	quick_sort(&mas[0], pivot, less);
 	quick_sort(&mas[pivot + 1], n - pivot - 1, less);
 }
@@ -56,8 +81,12 @@ int main() {
 	for(int i = 0; i < n; ++i) {
 		scanf("%d", &mas[i]);
 	}
+	//printf("\n");
 	quick_sort(mas, n, less);
 	printf("%d", mas[k]);
+	//for(int i = 0; i < n; ++i) {
+	//	printf("%d ", mas[i]);
+	//}
 	delete[] mas;
 	return 0;
 }
